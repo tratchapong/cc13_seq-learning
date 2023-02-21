5b0914fd9394f6b69675d68d91538d1e6e736d4c
tratchapong <tratchapong@gmail.com>
2023-02-22 06:17:35 +0700

    complete password compare in passport.js

--- project05/config/passport.js
+++ project05/config/passport.js
@@ -1,4 +1,5 @@
 const passport = require("passport");
+const bcrypt = require('bcryptjs')
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 const { User } = require("../models");
 const LocalStrategy = require('passport-local').Strategy
@@ -11,7 +12,8 @@ passport.use(new LocalStrategy({
   const user = await User.findOne({ where: {username : username}})
   if(!user)
     return cb(null, false)
-  if(password !== '1234')
+  let pw_ok = await bcrypt.compare(password, user.password)
+  if(!pw_ok)
     return cb(null, false)
   return cb(null, user)
 }))
